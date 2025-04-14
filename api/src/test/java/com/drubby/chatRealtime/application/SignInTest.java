577fbfdb34eac02fc9460094cb9b2bcab7d7bd55
juandgg <juanguiza65@gmail.com>
2025-04-14 17:54:23 -0500

    Faulty tests are added and need to be corrected

--- api/src/test/java/com/drubby/chatRealtime/application/SignInTest.java
+++ api/src/test/java/com/drubby/chatRealtime/application/SignInTest.java
@@ -1,13 +1,76 @@
 package com.drubby.chatRealtime.application;
 
-
+import com.drubby.chatRealtime.application.http.request.AuthBodyRequest;
+import com.drubby.chatRealtime.application.http.response.AuthResponse;
+import com.drubby.chatRealtime.application.http.response.msg.AuthMessage;
+import com.drubby.chatRealtime.application.useCase.SignInUseCase;
+import com.drubby.chatRealtime.application.useCase.UserDaoDetailsUseCase;
+import com.drubby.chatRealtime.domain.entity.UserEntity;
+import com.drubby.chatRealtime.infrastructure.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+
+@ExtendWith(MockitoExtension.class)
 public class SignInTest {
 
-    @Test
-    public void signIn() {
-        int i = 0 ;
 
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserDaoDetailsUseCase userDaoDetailsUseCase;
+
+
+    @InjectMocks
+    private SignInUseCase signInService;
+
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Test
+    public void signInValidCredentials() {
+
+        AuthBodyRequest authBodyRequest = new AuthBodyRequest();
+        authBodyRequest.setEmail("juanguiza65@gmail.com");
+        authBodyRequest.setPassword("juan1234");
+
+
+        UserEntity userEntity = UserEntity.builder()
+                .email("juanguiza65@gmail.com")
+                .passwordHash(passwordEncoder.encode("juan123"))
+                .accountNonExpired(true)
+                .accountNonLocked(true)
+                .createdAt(LocalDateTime.now())
+                .credentialsNonExpired(true)
+                .enabled(true)
+                .build();
+
+
+        when(userDaoDetailsUseCase.customLoadUserByEmail("juanguiza65@gmail.com", null))
+                .thenReturn((UserDetails) userEntity);
+
+
+        AuthResponse response = signInService.signIn(authBodyRequest);
+
+
+        assertNotNull(response);
+        assertEquals(HttpStatus.ACCEPTED, response.status());
+        assertEquals(AuthMessage.LOGIN_SUCCESS.getMessage(), response.authMessage());
+        assertNotNull(response.token());
     }
 }
